46a91c5ed63a21cd71dfe39525f2fcfb461e7a1e
Mehdi Asadli <mehdiasadli@azericard.com>
2025-10-22 12:01:38 +0400

    update: requirements on car details page

--- app/(main)/cars/rent/[id]/car-details-sections/requirements.tsx
+++ app/(main)/cars/rent/[id]/car-details-sections/requirements.tsx
@@ -1,3 +1,5 @@
+import { AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
+import { Accordion } from '@radix-ui/react-accordion';
 import { Info } from 'lucide-react';
 
 const requirements = [
@@ -10,19 +12,27 @@ const requirements = [
 
 export default function Requirements() {
   return (
-    <div className='bg-white border-2 border-gray-200 p-6'>
-      <div className='flex items-center gap-2 mb-4'>
-        <Info className='w-6 h-6 text-blue-600' strokeWidth={2} />
-        <h2 className='text-2xl font-bold text-gray-900'>Rental Requirements</h2>
-      </div>
-      <ul className='space-y-3'>
-        {requirements.map((req, index) => (
-          <li key={index} className='flex items-start gap-2 text-gray-700'>
-            <span className='text-primary font-bold mt-1'>•</span>
-            <span>{req}</span>
-          </li>
-        ))}
-      </ul>
+    <div className='bg-white border-2 border-gray-200 px-6 py-3'>
+      <Accordion type='single' collapsible>
+        <AccordionItem value='requirements'>
+          <AccordionTrigger>
+            <div className='flex items-center gap-2'>
+              <Info className='w-6 h-6 text-blue-600' strokeWidth={2} />
+              <h2 className='text-lg font-bold text-gray-900'>Rental Requirements</h2>
+            </div>
+          </AccordionTrigger>
+          <AccordionContent>
+            <ul className='space-y-3'>
+              {requirements.map((req, index) => (
+                <li key={index} className='flex items-center gap-2 text-gray-700'>
+                  <span className='text-primary font-bold'>•</span>
+                  <span>{req}</span>
+                </li>
+              ))}
+            </ul>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
