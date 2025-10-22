329f27c66804c654d4abcb585637b3b1e2e11d46
Mehdi Asadli <mehdiasadli@azericard.com>
2025-10-22 11:57:06 +0400

    remove: unused vars

--- components/navigation-elements/navigation-actions.tsx
+++ components/navigation-elements/navigation-actions.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import Link from 'next/link';
-import { Button } from '../ui/button';
 import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 import NavigationSearch from './navigation-search';
